978b84cc6cdd6c12bff2e15fd83091b41ce1cd5d
An Do <dovanminhan@gmail.com>
2026-08-19 21:16:11 +0700

    fix: :ambulance: fix build issue

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,29 +5,28 @@ import PageBuilder from '@/app/components/PageBuilder'
 import {sanityFetch} from '@/sanity/lib/live'
 import {getHomePageQuery, homeMetadataQuery, settingsQuery} from '@/sanity/lib/queries'
 import {resolveOpenGraphImage} from '@/sanity/lib/utils'
-import {Settings} from '@/sanity.types'
 
 export async function generateMetadata(): Promise<Metadata> {
   const [{data: page}, {data: settings}] = await Promise.all([
     sanityFetch({query: homeMetadataQuery, stega: false}),
     sanityFetch({query: settingsQuery, stega: false}),
   ])
-  const seo = page?.seo || (settings as Settings)?.defaultSeo
+  const seo = page?.seo || settings?.defaultSeo
   let openGraphImage = null
   try {
     openGraphImage = resolveOpenGraphImage(
       !!seo?.openGraphImage?.asset
         ? seo?.openGraphImage
-        : (settings as Settings)?.ogImage
-          ? (settings as Settings)?.ogImage
+        : settings?.ogImage
+          ? settings.ogImage
           : null,
     )
   } catch (error) {
     console.error('Error resolving Open Graph image:', error)
   }
   return {
-    title: seo?.metaTitle || page?.name || (settings as Settings)?.brandName,
-    description: seo?.metaDescription || (settings as Settings)?.footerIntro,
+    title: seo?.metaTitle || page?.name || settings?.brandName,
+    description: seo?.metaDescription || settings?.footerIntro,
     openGraph: {images: openGraphImage ? [openGraphImage] : []},
   }
 }
